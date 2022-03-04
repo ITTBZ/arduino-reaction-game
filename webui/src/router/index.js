c961c939d7521a26fca3dcf2007d4ef1219c0d08
taagomag <max.gordonsulzberger1@swisscom.com>
2022-03-04 14:29:05 +0100

    LoginPage, Leaderboard hardcode

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginPage from '../views/LoginPage.vue'
+import LeaderboardPage from '../views/LeaderboardPage.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: LoginPage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/leaderboard',
+    name: 'leaderboard',
+    component: LeaderboardPage
+  },
 ]
 
 const router = createRouter({
---
+++ webui/src/views/LeaderboardPage.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="leaderboardPage">
+    <h1>LeaderBoard</h1>
+    <div class="layout">
+      <div class="leaderboard">
+        <table class="styled-table">
+          <thead>
+            <tr>
+              <th>username</th>
+              <th>score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Max</td>
+              <td>17</td>
+            </tr>
+            <tr class="active-row">
+              <td>Dillan</td>
+              <td>358</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+//import store from "../store/index.js";
+export default {
+  name: "LeaderboardPage",
+  data() {
+    return {
+      username: "username",
+      score: 0,
+    };
+  },
+};
+</script>
+<style>
+body {
+  background-color: rgb(216, 216, 216);
+  height: 100%;
+  width: 100%;
+}
+.layout {
+  display: flex;
+  justify-content: center;
+}
+.styled-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.styled-table thead tr {
+    background-color: #009879;
+    color: #ffffff;
+}
+.styled-table tbody td,
+.styled-table thead tr {
+  text-align: left; 
+}
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
+}
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+}
+.styled-table tbody tr.active-row {
+    font-weight: bold;
+    color: #009879;
+}
+</style>
