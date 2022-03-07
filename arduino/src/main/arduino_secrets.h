a7a03080cb20cab5418966cf97be519284297b5f
GeneTv <contact@genetv.net>
2022-03-07 18:56:16 +0100

    Hmm

---
+++ arduino/src/main/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "LERNKUBE"
+#define SECRET_PASS "l3rnk4b3"
--- arduino/src/main/main.ino
+++ arduino/src/main/main.ino
@@ -1,21 +1,32 @@
-// -----[ DEFINE PINS ]-----
-int LED_PIN = 6;
-int BUTTON_PIN = 3;
+#include <ArduinoHttpClient.h>
+#include <SPI.h>
+#include <WiFiNINA.h>
 
-//-----[ DEFINE HARDWARE SPECIFIC ]-----//
-int BUTTON_INTERRUPT_DELAY = 20;
-int PIXEL_COUNT = 60;
-
-//-----[ SETUP NEOPIXEL ]-----//
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
+int LED_PIN = 6;
+int BUTTON_PIN = 3;
+int BUTTON_INTERRUPT_DELAY = 20;
+int PIXEL_COUNT = 60;
+
+// Neopixel config
 int PIXEL_FORMAT = NEO_GRB + NEO_KHZ800;
 Adafruit_NeoPixel *pixels;
 
-//===========[ run the main code ]===========//
+// Initialize WiFi network credentials
+#include "arduino_secrets.h"
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, "195.201.132.250", 3000);
+int wifiStatus = WL_IDLE_STATUS;
+
+String apiToken = "";
+String tempCode = "";
 
 void setup() {
   Serial.begin(9600);
@@ -25,6 +36,15 @@ void setup() {
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonPressedAction, CHANGE);
+
+  while ( wifiStatus != WL_CONNECTED) {
+    Serial.println("Connecting...");
+    wifiStatus = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("Connected to: " + WiFi.SSID() + " as " + WiFi.localIP());
+
+  // TODO Generate tempCode
 }
 
 void setLightColor(int r, int g, int b) {
@@ -51,6 +71,17 @@ void loop() {
   switch(game_state) {
     case loggedOut:
       setLightColor(150, 150, 25);
+      delay(1000);
+      client.get("/identity?code=" + tempCode);
+    
+      int statusCode = client.responseStatusCode();
+      String response = client.responseBody();
+
+      if(statusCode == 200) {
+        apiToken = response;
+        game_state = idle;
+        Serial.println(response);
+      }
       break;
     case idle:
       setLightColor(0, 0, 150);
@@ -65,8 +96,6 @@ void loop() {
       setLightColor(0, 150, 0);
       break;
   }
-
-  // TODO: Show account id on display, if logged in
 }
 
 static unsigned long last_interrupt_time = 0;
@@ -90,12 +119,6 @@ void buttonPressedAction() {
 
   // Now we can continue with the normal code execution
   switch(game_state) {
-    case loggedOut:
-      // User is not logged in... Handle the login on button click for now
-      // TODO: Handle login -> This should happen over the fingerprint reader instead of the button press.
-      game_state = idle;
-      break;
-
     case idle:
       game_state = btnNotReady;
       break;
@@ -109,6 +132,17 @@ void buttonPressedAction() {
       game_state = idle;
       int reaction_time = current_time - reaction_accepted_since;
       Serial.println(reaction_time);
+
+      String postData = "{\"score\": " + reaction_time + "}";
+      client.beginRequest();
+      client.post("/games");
+      client.sendHeader("Content-Type", "application/json");
+      client.sendHeader("Content-Length", postData.length());
+      client.sendHeader("Authorization", "Bearer " + apiToken);
+      client.beginBody();
+      client.print(postData);
+      client.endRequest();
+      // POST REQUEST
       break;
   }
 }
