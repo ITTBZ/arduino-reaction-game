4ed51ccdd8362132abedc8abd99e17c4b67ae62d
GeneTv <contact@genetv.net>
2022-03-07 17:02:35 +0100

    Yee

--- arduino/src/main/main.ino
+++ arduino/src/main/main.ino
@@ -1,3 +1,7 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+
 // -----[ DEFINE PINS ]-----
 int LED_PIN = 6;
 int BUTTON_PIN = 3;
@@ -6,25 +10,63 @@ int BUTTON_PIN = 3;
 int BUTTON_INTERRUPT_DELAY = 20;
 int PIXEL_COUNT = 60;
 
-//-----[ SETUP NEOPIXEL ]-----//
+//-----[ INIT NEOPIXEL ]-----//
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
+//-----[ INIT WIFI ]-----//
+
+
+#include "arduino_secrets.h" 
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+int status = WL_IDLE_STATUS;
+
+//-----[ REST OF INIT ]-----//
 int PIXEL_FORMAT = NEO_GRB + NEO_KHZ800;
 Adafruit_NeoPixel *pixels;
+WiFiClient client;
 
-//===========[ run the main code ]===========//
 
+//===========[ run the main setup code ]===========//
 void setup() {
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
   
   pixels = new Adafruit_NeoPixel(PIXEL_COUNT, LED_PIN, PIXEL_FORMAT);
   pixels->begin();
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonPressedAction, CHANGE);
+
+  // Wifi things
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.println("Connected to WiFi");
 }
 
 void setLightColor(int r, int g, int b) {
@@ -47,10 +89,38 @@ GameState game_state = loggedOut;
 
 unsigned int reaction_accepted_since = 0; // This value inicates since when the leds are glowing green and the program accepts an input
 
+//===========[ run the main code ]===========//
 void loop() {
   switch(game_state) {
     case loggedOut:
       setLightColor(150, 150, 25);
+
+      String tempCode = "A5HK92";
+
+      if (client.connect("192.168.96.239", 3000)) {
+        Serial.println("connected to server");
+        // Make a HTTP request:
+        client.println("GET /search?q=" + tempCode + " HTTP/1.1");
+        client.println("Host: 192.168.96.239");
+        client.println("Connection: close");
+        client.println();
+      }
+
+      while (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+      }
+
+      // if the server's disconnected, stop the client:
+      if (!client.connected()) {
+        Serial.println();
+        Serial.println("disconnecting from server.");
+        client.stop();
+
+        // do nothing forevermore:
+        while (true);
+      }
+
       break;
     case idle:
       setLightColor(0, 0, 150);
